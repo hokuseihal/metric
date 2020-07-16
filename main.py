68b94fc89e8915e4012bc1957b98e6a540c84775
Naoki Wada <hokuseihal@gmail.com>
2020-07-16 19:20:53 +0900

    fix arcface

--- main.py
+++ main.py
@@ -5,50 +5,57 @@ from multiprocessing import cpu_count
 from torchvision.models import mobilenet_v2
 from torchmeta.datasets.helpers import omniglot
 from torchmeta.utils.data import BatchMetaDataLoader
-from core import savedic,addvalue
+from core import savedic, addvalue
 from net.arcface import Arcface
 from net.radam import RAdam
+from core import addvalue, savedic
+
 
 def operate():
-    for bch,batch in enumerate(dataloader):
-        # print(f'{bch}/{len(dataloader)}:{e}')
-        for phase in ['train','test']:
-            data,target=batch[phase]
-            data=data.repeat(1,1,3,1,1)
-            B,ShotWay,C,H,W=data.shape
-            assert ShotWay==Shot*Way
-            data=data.to(device).reshape(B,Way,Shot,C,H,W).permute(0,2,1,3,4,5).reshape(-1,C,H,W)
-            target=target.to(device).reshape(B,Way,Shot).permute(0,2,1)
+    for bch, batch in enumerate(dataloader):
+        if bch>batch_on_epoch:
+            break
+        for phase in ['train', 'test']:
+            with torch.set_grad_enabled(phase == 'train'):
+                data, target = batch[phase]
+                data = data.repeat(1, 1, 3, 1, 1)
+                B, ShotWay, C, H, W = data.shape
+                assert ShotWay == Shot * Way
+                data = data.to(device).reshape(B, Way, Shot, C, H, W).permute(0, 2, 1, 3, 4, 5).reshape(-1, C, H, W)
+                target = target.to(device).reshape(B, Way, Shot).permute(0, 2, 1)
 
-            output=model(data)
-            output=output.reshape(B,Shot,Way,-1)
-            output,target=arcface(output,target)
+                output = model(data)
+                output = output.reshape(B, Shot, Way, -1)
+                output, target = arcface(output, target)
 
-            loss=lossf(output,target)
-            print(f'{loss.item():.4f}')
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
+                loss = lossf(output, target)
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+                with torch.no_grad():
+                    acc = (output.argmax(-1) == target).sum().float() / len(target)
+                print(f'{e}:{bch}/{min(batch_on_epoch,len(dataloader))}, {loss.item():.4f}, {acc.item() * 100:.2f}%')
+
+                addvalue(writer, f'loss:{phase}', loss.item(), e)
+                addvalue(writer, f'acc:{phase}', acc.item(), e)
 
 
-            addvalue(writer,f'loss:{phase}',loss.item(),e)
-            # acc=accf(output,target)
-            # addvalue(writer,f'acc:{phase}',acc.item(),e)
-
-if __name__=='__main__':
-    device='cuda' if torch.cuda.is_available() else 'cpu'
-    Way=2
-    Shot=3
-    batchsize=32
-    dataset = omniglot("../data/metric", ways=Way, shots=Shot, test_shots=Shot, meta_train=True)
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    Way = 2
+    Shot = 3
+    batchsize = 256
+    batch_on_epoch=16
+    dataset = omniglot("../data/metric/", ways=Way, shots=Shot, test_shots=Shot, meta_train=True, download=True)
     dataloader = BatchMetaDataLoader(dataset, batch_size=batchsize, num_workers=cpu_count())
-    lossf=nn.CrossEntropyLoss()
-    num_epoch=100
-    writer={}
-    model=mobilenet_v2().to(device)
-    arcface=Arcface()
-    optimizer=RAdam(model.parameters())
+    lossf = nn.CrossEntropyLoss()
+    num_epoch = 100
+    writer = {}
+    model = mobilenet_v2().to(device)
+    arcface = Arcface()
+    optimizer = RAdam(model.parameters())
 
     for e in range(num_epoch):
         operate()
-        savedic(writer,'data')
+        savedic(writer, 'data')
--- net/arcface.py
+++ net/arcface.py
@@ -1,21 +1,32 @@
 import torch
 import torch.nn as nn
 from itertools import combinations
+
+
+def impose(x, m1=1., m2=0., m3=0.):
+    # TODO implement m1
+    return x * torch.tensor(m2).cos() - (1 - x ** 2).sqrt() * torch.tensor(m2).sin() - m3
+
+
 class Arcface(nn.Module):
     def __init__(self):
         super(Arcface, self).__init__()
-    def forward(self,x,target):
-        s=5
-        ret_x=[]
-        ret_target=[]
-        #x:(B,S,W,h)
-        B,S,W,h=x.shape
-        #label:(B,S,W)
-        x=x/x.norm()
+
+    def forward(self, x, target):
+        s = 5
+        ret_x = []
+        ret_target = []
+        # x:(B,S,W,h)
+        B, S, W, h = x.shape
+        # label:(B,S,W)
+        x = x / ((x ** 2).sum(-1).sqrt().view(B, S, W, 1))
         for b in range(B):
-            for comb in combinations(range(S),2):
-                x0,x1=x[b,comb[0]],x[b,comb[1]]
-                t0,t1=target[b,comb[0]],target[b,comb[1]]
-                ret_x+=[(x0@x1.T)*s]
-                ret_target+=[(t0.view(-1,1)==t1.view(1,-1)).int().argmax(-1)]
-        return torch.cat(ret_x,dim=0),torch.cat(ret_target,dim=0)
+            for comb in combinations(range(S), 2):
+                x0, x1 = x[b, comb[0]], x[b, comb[1]]
+                t0, t1 = target[b, comb[0]], target[b, comb[1]]
+                ret_target += [(t0.view(-1, 1) == t1.view(1, -1)).int().argmax(-1)]
+                _x = (x0 @ x1.T) * s
+                mask = (t0.view(-1, 1) == t1.view(1, -1))
+                # _x[mask]=impose(_x[mask])
+                ret_x += [_x]
+        return torch.cat(ret_x, dim=0), torch.cat(ret_target, dim=0)
