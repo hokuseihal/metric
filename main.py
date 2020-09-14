b020a6b1ce1a2da3278b3fd998291f621bc365c0
Naoki Wada <hokuseihal@gmail.com>
2020-09-14 10:29:07 +0900

    add mini imagenet

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from multiprocessing import cpu_count
 from torchvision.models import mobilenet_v2
-from torchmeta.datasets.helpers import omniglot
+from torchmeta.datasets.helpers import omniglot,cifar_fs
 from torchmeta.utils.data import BatchMetaDataLoader
 from core import savedic, addvalue
 from net.arcface import Arcface
@@ -18,7 +18,7 @@ def operate():
         for phase in ['train', 'test']:
             with torch.set_grad_enabled(phase == 'train'):
                 data, target = batch[phase]
-                data = data.repeat(1, 1, 3, 1, 1)
+                # data = data.repeat(1, 1, 3, 1, 1)
                 B, ShotWay, C, H, W = data.shape
                 assert ShotWay == Shot * Way
                 data = data.to(device).reshape(B, Way, Shot, C, H, W).permute(0, 2, 1, 3, 4, 5).reshape(-1, C, H, W)
@@ -35,7 +35,7 @@ def operate():
                     optimizer.zero_grad()
                 with torch.no_grad():
                     acc = (output.argmax(-1) == target).sum().float() / len(target)
-                print(f'{e}:{bch}/{min(batch_on_epoch,len(dataloader))}, {loss.item():.4f}, {acc.item() * 100:.2f}%')
+                print(f'{phase},{e}:{bch}/{min(batch_on_epoch,len(dataloader))}, {loss.item():.4f}, {acc.item() * 100:.2f}%')
 
                 addvalue(writer, f'loss:{phase}', loss.item(), e)
                 addvalue(writer, f'acc:{phase}', acc.item(), e)
@@ -44,10 +44,11 @@ def operate():
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     Way = 2
-    Shot = 3
+    Shot = 2
     batchsize = 256
     batch_on_epoch=16
-    dataset = omniglot("../data/metric/", ways=Way, shots=Shot, test_shots=Shot, meta_train=True, download=True)
+    # dataset = omniglot("../data/metric/", ways=Way, shots=Shot, test_shots=Shot, meta_train=True, download=True)
+    dataset = cifar_fs("../data/metric/", ways=Way, shots=Shot, test_shots=Shot, meta_train=True, download=True)
     dataloader = BatchMetaDataLoader(dataset, batch_size=batchsize, num_workers=cpu_count())
     lossf = nn.CrossEntropyLoss()
     num_epoch = 100
